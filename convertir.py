1806131e32944ce407d203e165a55137cfacc952
Fernando <femastro@gmail.com>
2023-01-27 05:04:05 -0300

    --amend

--- convertir.py
+++ convertir.py
@@ -2,15 +2,16 @@ import os
 import time
 from colorama import init, Fore
 
+
 class bcolors:
-    WHITE = '\033[2;37m' #WHITE
-    PURPLE = '\033[0;35m' #PURPLE
-    GREEN = '\033[0;92m' #GREEN
-    CYAN = '\033[0;36m' #CYAN
-    YELLOW = '\033[0;93m' #YELLOW
-    RED = '\033[0;91m' #RED
-    BLUE = '\033[0;34m' #BLUE
-    RESET = '\033[0;0m' #RESET COLOR
+    WHITE = '\033[2;37m'  # WHITE
+    PURPLE = '\033[0;35m'  # PURPLE
+    GREEN = '\033[0;92m'  # GREEN
+    CYAN = '\033[0;36m'  # CYAN
+    YELLOW = '\033[0;93m'  # YELLOW
+    RED = '\033[0;91m'  # RED
+    BLUE = '\033[0;34m'  # BLUE
+    RESET = '\033[0;0m'  # RESET COLOR
 
 
 def my_funcion(data):
@@ -33,11 +34,12 @@ def downvideo():
     print(bcolors.YELLOW+'DESCARGAR VIDEO COMPLETO'+bcolors.GREEN)
     print('========================')
     print('')
-    url = input(bcolors.CYAN+"Ingrese URL del Video de Youtube ( "+bcolors.PURPLE+"0 para Volver al Menú "+bcolors.CYAN+")"+bcolors.GREEN+" \n \n -> ")
+    url = input(bcolors.CYAN+"Ingrese URL del Video de Youtube ( "+bcolors.PURPLE +
+                "0 para Volver al Menú "+bcolors.CYAN+")"+bcolors.GREEN+" \n \n -> ")
     if len(url) < 3:
         init()
     else:
-        video = "youtube-dl --format mp4 " + url
+        video = "youtube-dl --format mp4 " + "'" + url + "'"
     my_funcion(video)
 
 
@@ -47,22 +49,25 @@ def downaudio():
     print(bcolors.YELLOW+'DESCARGAR AUDIO de un VIDEO'+bcolors.RESET)
     print('===========================')
     print('')
-    url = input(bcolors.CYAN+"Ingrese URL del Video de Youtube ( "+bcolors.PURPLE+"0 para Volver al Menú "+bcolors.CYAN+")"+bcolors.GREEN+" \n \n -> ")
+    url = input(bcolors.CYAN+"Ingrese URL del Video de Youtube ( "+bcolors.PURPLE +
+                "0 para Volver al Menú "+bcolors.CYAN+")"+bcolors.GREEN+" \n \n -> ")
     if len(url) < 3:
         init()
     else:
-        video = "youtube-dl -x --audio-format mp3 " + url 
+        video = "youtube-dl -x --audio-format mp3 " + "'" + url + "'"
     my_funcion(video)
 
 
 def init():
     os.system('clear')
-    print(Fore.LIGHTGREEN_EX+'\n') ## Usando Colorama 
+    print(Fore.LIGHTGREEN_EX+'\n')  # Usando Colorama
     print('############################################')
     print('##                                        ##')
-    print('##'+bcolors.PURPLE+'               BIENVENIDO               '+bcolors.GREEN+'##')
+    print('##'+bcolors.PURPLE +
+          '               BIENVENIDO               '+bcolors.GREEN+'##')
     print('##                                        ##')
-    print('##'+bcolors.YELLOW+'  DESCARGAS VIDEOS y AUDIOS DE YOUTUBE  '+bcolors.GREEN+'##')
+    print('##'+bcolors.YELLOW +
+          '  DESCARGAS VIDEOS y AUDIOS DE YOUTUBE  '+bcolors.GREEN+'##')
     print('##                                        ##')
     print('############################################')
     print(bcolors.WHITE+'                               by Mastrosoft')
@@ -99,19 +104,17 @@ def salir():
     print('##'+bcolors.YELLOW+'     SALIENDO del PROGRAMA    '+bcolors.GREEN+'##')
     print('##################################')
     print('\n')
-    print (bcolors.RED+"Programa Cerrado", end=" "+bcolors.GREEN)
+    print(bcolors.RED+"Programa Cerrado", end=" "+bcolors.GREEN)
     for i in range(15):
         if i < 14:
             print(".", end="")
             x = 0
             while x < 3000:
-                x+=1
+                x += 1
     time.sleep(1)
     print("\n")
     exit()
 
 
-#### Initial Program
+#  Initial Program
 init()
-
-
